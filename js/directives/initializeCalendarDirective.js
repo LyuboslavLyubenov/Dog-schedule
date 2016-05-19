3427dc09098a87a0f1464be51365f9d709d1c309
Lyuboslav Shishkov <lubidead1@mail.bg>
2016-05-19 23:58:39 +0300

    Added dialogbox for event completition
    *not working still

--- js/directives/initializeCalendarDirective.js
+++ js/directives/initializeCalendarDirective.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     angular
         .module('DogWalkingSchedule')
         .directive('initializeCalendar', ['$kinvey',
@@ -18,21 +18,77 @@
 
                 function link(scope, element, attrs) {
                     var events = $kinvey.Object('Events');
-                    var allEvents = events.query({
+
+                    events.query({
                         query: {
                             assignee: 'dead4y'
                         }
                     }).$promise.then(function (success) {
                         $(element).fullCalendar({
-                            header:
-                            {
+                            header: {
                                 left: '',
                                 right: ''
                             },
                             editable: false,
                             defaultView: 'agendaWeek',
                             lang: 'bg',
-                            events: convertEventsData(success)
+                            events: convertEventsData(success),
+                            scope: {},
+                            eventClick: function (calEvent, jsEvent, view) {
+                                const errorMessage = 'Проблем при връзката със сървъра. Моля провери интернет връзката си и викни любо!!!';
+
+                                var startDate = moment(calEvent.start).calendar(),
+                                    endDate = moment(calEvent.end).calendar();
+
+                                alertify.confirm().setting({
+                                    labels: {
+                                        ok: 'Окей',
+                                        cancel: 'Не съм сигурен'
+                                    },
+                                    title: '',
+                                    message: 'Потвърждаваш ли че ще разходиш кучето от ' + startDate + ' до ' + endDate,
+                                    modal: true,
+                                    movable: false,
+                                    resizable: false,
+                                    onok: onOK
+                                }).show();
+
+
+
+                                function onOK() {
+                                    //TODO
+                                    var allEvents = $kinvey.Object('Events'),
+                                        startDate = formatDate(calEvent.start),
+                                        endDate = formatDate(calEvent.end),
+                                        username = sessionStorage.getItem('username'),
+                                        event = allEvents.query({
+                                            query: {
+                                                start: startDate,
+                                                end: endDate,
+                                                assignee: username
+                                            }
+                                        });
+
+                                    function formatDate(date) {
+                                        var result = moment(date).format();
+                                        return result.slice(0, result.length - 1);
+                                    }
+
+                                    event.$promise.then(function (success) {
+                                        success[0].isCompleted = true;
+                                        success.$save().$promise.then(function () {
+                                            alertify.notify('Успешно изпълни задача! Браво!', 'success', 5);
+                                        }, function (error) {
+                                            alertify.notify(errorMessage, 'error', 7);
+                                            console.error(error);
+                                        });
+                                    }, function (error) {
+                                        alertify.notify(errorMessage, 'error', 7);
+                                        console.error(error);
+                                    });
+
+                                }
+                            }
                         });
 
                     }, function (error) {
